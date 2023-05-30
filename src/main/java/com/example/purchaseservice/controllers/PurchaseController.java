19a0afc8ed1832ea6e15eb8bdb684093312f3db4
Mark-Masomi <moinmirmasomi@outlook.com>
2023-05-30 16:49:37 +0200

    ....

--- src/main/java/com/example/purchaseservice/controllers/PurchaseController.java
+++ src/main/java/com/example/purchaseservice/controllers/PurchaseController.java
@@ -4,6 +4,7 @@ import com.example.purchaseservice.models.Purchase;
 import com.example.purchaseservice.DTO.Customer;
 import com.example.purchaseservice.DTO.Item;
 import com.example.purchaseservice.repos.PurchaseRepo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -45,56 +46,41 @@ public class PurchaseController {
         return purchaseRepo.findAll();
     }
 
+    @Value("${item-service.baseurl}")
+    private String itemServiceBaseUrl;
+    @Value("${customer-service.baseurl}")
+    private String customerServiceBaseUrl;
 
     @PostMapping("/newbypost/{customerid}/{itemid}")
     public String newPurchaseByPost(@PathVariable Long customerid,@PathVariable Long itemid ){
-        String CostumerServiceUr1="http://localhost:8081/customers/"+customerid;
+        String CostumerServiceUr1=customerServiceBaseUrl+"/customers/"+customerid;
         Customer customer=restTemplate.getForObject(CostumerServiceUr1, Customer.class);
 
         Date currentDate=new Date(System.currentTimeMillis());
 
-        String itemServiceUr1="http://localhost:8082/items/"+itemid;
+        String itemServiceUr1=itemServiceBaseUrl+ "/items/"+itemid;
+        System.out.println(itemServiceUr1);
         Item item=restTemplate.getForObject(itemServiceUr1, Item.class);
-        //CUSTOMER ID INSTEAD , ITEM ID TOO
         Purchase purchase=new Purchase(currentDate,customer.getId(),item.getId());
         purchaseRepo.save(purchase);
         return "The purchase was added to database";
     }
-    /*
+
     @RequestMapping("/newbyget/{customerid}/{itemid}")
     public String newPurchaseByget(@PathVariable Long customerid,@PathVariable Long itemid ){
-        Customer customer=customerRepo.findById(customerid).get();
-        Item item=itemRepo.findById(itemid).get();
-        List<Item> itemsList=new ArrayList<>();
-        itemsList.add(item);
+        String CostumerServiceUr1=customerServiceBaseUrl+"/customers/"+customerid;
+        Customer customer=restTemplate.getForObject(CostumerServiceUr1, Customer.class);
+
+        String itemServiceUr1=itemServiceBaseUrl+"/items/"+itemid;
+        Item item=restTemplate.getForObject(itemServiceUr1, Item.class);
+
+
         Date currentDate=new Date(System.currentTimeMillis());
 
-        Purchase purchase=new Purchase(currentDate,customer,itemsList);
+        Purchase purchase=new Purchase(currentDate,customer.getId(),item.getId());
         purchaseRepo.save(purchase);
         return "The purchase was added to database";
-    }*/
-    /*
-    @RequestMapping("/moreitems/{purchaseid}/{newitemsid}")
-    public String addMoreItemsToPurchase(@PathVariable Long purchaseid,@PathVariable Long newitemsid){
-        Purchase existingPurchase=purchaseRepo.findById(purchaseid).get();
-        String itemServiceUr1="http://localhost:8082/items/"+newitemsid;
-        Item newitem=restTemplate.getForObject(itemServiceUr1, Item.class);
-        List<Item> itemList=existingPurchase.getItemsList();
-        itemList.add(newitem);
-        existingPurchase.setItemsList(itemList);
-        if (existingPurchase !=null){
-        purchaseRepo.save(existingPurchase);
-        }
-        return "The "+newitem.getName()+" was added to the existing purchase";
     }
-    @RequestMapping("/forcustomer/{customerid}")
-    public List<Purchase> customerPurchases(@PathVariable Long customerid){
-        String customerur1="http://localhost:8080/customers/"+customerid;
-        Customer customer=restTemplate.getForObject(customerur1, Customer.class);
-
-        return purchaseRepo.findbyCustomer(customer);
-
-    }*/
 
 
 
